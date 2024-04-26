51c2d447ba95f3297662ea65aa8c7c9c9e54eb8f
Pavel Buryka <l1kaf@yandex.ru>
2024-04-26 14:19:02 +0300

    update post CRUD

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -21,7 +21,7 @@
             </li>
             <li class="nav-item">
                 <a href="{{ route('admin.post.index') }}" class="nav-link">
-                    <i class="nav-icon fas fa-tag"></i>
+                    <i class="nav-icon far fa-clipboard"></i>
                     <p>
                         Посты
                     </p>
