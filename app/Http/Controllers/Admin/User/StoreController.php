bde1464083ccb5a0f90ed7cd0998b6d3ed3ed549
Pavel Buryka <l1kaf@yandex.ru>
2024-05-02 19:27:28 +0300

    add jobs create user

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\User\StoreRequest;
+use App\Jobs\StoreUserJob;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
@@ -12,8 +13,7 @@ class StoreController extends Controller
     public function __invoke(StoreRequest $request)
     {
         $data = $request->validated();
-        $data['password'] = Hash::make($data['password']);
-        User::firstOrCreate(['email' => $data['email']], $data);
+        StoreUserJob::dispatch($data);
         return redirect()->route('admin.user.index');
     }
 }
